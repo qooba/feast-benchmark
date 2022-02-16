597cdf84e6e7651cbe58dc6441fd7f9d0911c49a
qooba <dev@qooba.net>
2022-02-16 02:17:34 +0100

    add dev

--- docker/dev.sh
+++ docker/dev.sh
@@ -3,4 +3,4 @@
 # BUILD DOCKER IMAGES
 #docker build -t qooba/feast:ray_dev -f Dockerfile.ray_dev .
 
-docker run -it -v $(pwd)/../../AIDaskFeast/src/feast:/feast-qooba qooba/feast:ray_dev /bin/bash
+docker run -it --rm --network app_default --name feast -p 8888:8888 -v $(pwd)/dataset:/app/dataset --shm-size=5.09gb -v $(pwd)/../../AIDaskFeast/src/feast:/feast-qooba qooba/feast:ray_dev /bin/bash
